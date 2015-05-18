ed041bdf410ad250af76969f68acbcb63e267c9b
Luke Chatton <snicktrix@gmail.com>
2015-05-18 16:04:51 -0700

    Add divional armor

--- src/com/minehut/pvp/Listeners/BukkitListeners.java
+++ src/com/minehut/pvp/Listeners/BukkitListeners.java
@@ -5,17 +5,17 @@ import com.minehut.api.util.player.GamePlayer;
 import com.minehut.api.util.player.Rank;
 import com.minehut.commons.common.chat.C;
 import com.minehut.commons.common.chat.F;
+import com.minehut.commons.common.items.ItemStackFactory;
 import com.minehut.commons.common.level.Level;
 import com.minehut.commons.common.player.PlayerUtil;
+import com.minehut.pvp.ELOManager;
 import com.minehut.pvp.arena.Arena.Team;
 import com.minehut.pvp.Core;
 
 import com.minehut.pvp.events.EventCaller;
 import com.minehut.pvp.events.events.SpawnPreparePlayerEvent;
 import com.minehut.pvp.stats.DeathStat;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.WeatherType;
+import org.bukkit.*;
 import org.bukkit.entity.Fish;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -47,9 +47,43 @@ public class BukkitListeners implements Listener {
 
     @EventHandler
     public void onSpawnPreparePlayer(SpawnPreparePlayerEvent event) {
-        event.getPlayer().setPlayerWeather(WeatherType.CLEAR);
-        PlayerUtil.clearAll(event.getPlayer());
-        event.getPlayer().getInventory().setItem(1, this.core.getGuiMenus().getQueueItem());
+        Player player = event.getPlayer();
+        player.setPlayerWeather(WeatherType.CLEAR);
+        PlayerUtil.clearAll(player);
+        player.getInventory().setItem(1, this.core.getGuiMenus().getQueueItem());
+
+        /* Divisional Armor */
+        ELOManager.Division division = this.core.getEloManager().getPlayerDivisionForElo(this.core.eloManager.getHighestELOAsELO(player));
+        if (division == ELOManager.Division.Bronze) {
+            player.getInventory().setHelmet(ItemStackFactory.createItem(Material.LEATHER_HELMET, C.yellow + "Bronze Division Helmet"));
+            player.getInventory().setChestplate(ItemStackFactory.createItem(Material.LEATHER_CHESTPLATE, C.yellow + "Bronze Division Chest Plate"));
+            player.getInventory().setLeggings(ItemStackFactory.createItem(Material.LEATHER_LEGGINGS, C.yellow + "Bronze Division Leggings"));
+            player.getInventory().setBoots(ItemStackFactory.createItem(Material.LEATHER_BOOTS, C.yellow + "Bronze Division Boots"));
+        }
+        else if (division == ELOManager.Division.Silver) {
+            player.getInventory().setHelmet(ItemStackFactory.createItem(Material.IRON_HELMET, C.yellow + "Iron Division Helmet"));
+            player.getInventory().setChestplate(ItemStackFactory.createItem(Material.IRON_CHESTPLATE, C.yellow + "Iron Division Chest Plate"));
+            player.getInventory().setLeggings(ItemStackFactory.createItem(Material.IRON_LEGGINGS, C.yellow + "Iron Division Leggings"));
+            player.getInventory().setBoots(ItemStackFactory.createItem(Material.IRON_BOOTS, C.yellow + "Iron Division Boots"));
+        }
+        else if (division == ELOManager.Division.Gold) {
+            player.getInventory().setHelmet(ItemStackFactory.createItem(Material.GOLD_HELMET, C.yellow + "Gold Division Helmet"));
+            player.getInventory().setChestplate(ItemStackFactory.createItem(Material.GOLD_CHESTPLATE, C.yellow + "Gold Division Chest Plate"));
+            player.getInventory().setLeggings(ItemStackFactory.createItem(Material.GOLD_LEGGINGS, C.yellow + "Gold Division Leggings"));
+            player.getInventory().setBoots(ItemStackFactory.createItem(Material.GOLD_BOOTS, C.yellow + "Gold Division Boots"));
+        }
+        else if (division == ELOManager.Division.Platinum) {
+            player.getInventory().setHelmet(ItemStackFactory.createColoredArmorWithGlow(Material.LEATHER_HELMET, C.yellow + "Platinum Division Helmet", Color.FUCHSIA));
+            player.getInventory().setChestplate(ItemStackFactory.createColoredArmorWithGlow(Material.LEATHER_CHESTPLATE, C.yellow + "Platinum Division Chest Plate", Color.FUCHSIA));
+            player.getInventory().setLeggings(ItemStackFactory.createColoredArmorWithGlow(Material.LEATHER_LEGGINGS, C.yellow + "Platinum Division Leggings", Color.FUCHSIA));
+            player.getInventory().setBoots(ItemStackFactory.createColoredArmorWithGlow(Material.LEATHER_BOOTS, C.yellow + "Platinum Division Boots", Color.FUCHSIA));
+        }
+        else if (division == ELOManager.Division.Diamond) {
+            player.getInventory().setHelmet(ItemStackFactory.createItemWithGlow(Material.DIAMOND_HELMET, C.yellow + "Diamond Division Helmet"));
+            player.getInventory().setChestplate(ItemStackFactory.createItemWithGlow(Material.DIAMOND_CHESTPLATE, C.yellow + "Diamond Division Chest Plate"));
+            player.getInventory().setLeggings(ItemStackFactory.createItemWithGlow(Material.DIAMOND_LEGGINGS, C.yellow + "Diamond Division Leggings"));
+            player.getInventory().setBoots(ItemStackFactory.createItemWithGlow(Material.DIAMOND_BOOTS, C.yellow + "Diamond Division Boots"));
+        }
     }
 
     @EventHandler
@@ -154,6 +188,8 @@ public class BukkitListeners implements Listener {
 
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
+        event.setQuitMessage("");
+
         if (this.core.queueManager.isPlayerInQueue(event.getPlayer().getUniqueId())) {
         	this.core.queueManager.leaveQueue(event.getPlayer().getUniqueId());
         }
@@ -169,6 +205,8 @@ public class BukkitListeners implements Listener {
     
     @EventHandler
     public void onKick(PlayerKickEvent event) {
+        event.setLeaveMessage("");
+
         if (this.core.queueManager.isPlayerInQueue(event.getPlayer().getUniqueId())) {
         	this.core.queueManager.leaveQueue(event.getPlayer().getUniqueId());
         }
